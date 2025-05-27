41632cfa792be0ed70258276e6efeb4f6d530574
Franco Fazzolari <fazzolari.franco.22@gmail.com>
2025-05-27 02:44:14 +0200

    segundoCommitTest

---
+++ src/components/ComponenteHeaderTest.jsx
@@ -0,0 +1,11 @@
+export default function ComponenteHeaderTest () {
+
+
+    return(
+        <div>
+            <h2>
+                Texto Header app de compras con carrito
+            </h2>
+        </div>
+    );
+}
