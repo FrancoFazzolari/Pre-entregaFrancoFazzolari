8ffc9ae3885535d3bc657e95e695fa2bf6a606f9
Franco Fazzolari <fazzolari.franco.22@gmail.com>
2025-05-30 03:10:51 +0200

    sexto commit entonces?

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,34 @@ import ComponenteHeaderTest from './components/ComponenteHeaderTest'
 import './App.css'
 import Button from 'react-bootstrap/Button'
 import Accordion from 'react-bootstrap/Accordion';
+import js from '@eslint/js';
 
 function App() {
   const listaDigimonLarga = "https://digimon-api.vercel.app/api/digimon/";
   const listaDigimonCorta = "https://digimon-api.vercel.app/api/digimon/level/in%20training";
-  const [count, setCount] = useState(0);
+  const listaDragonBallZ = "https://dragonball-api.com/api/characters?limit=14";
   const [digimones, setDigimones] = useState([]);
   const [cargandoDatos, setCargandoDatos] = useState(true);
 
   useEffect(
+    ()=> {
+      const obtenerDatosApiListaLarga = async()=> {
+        const resultadoApiFetch = await fetch(listaDigimonLarga);
+        console.log("probando obtencion datos" + resultadoApiFetch.json()
+        .then(json=>{
+          console.log("probando luego de convertir a Json"+json);
+        })
+      );
+      }
+      obtenerDatosApiListaLarga();
+    }
+  , []);
+  useEffect(
     ()=>{
-      fetch(listaDigimonCorta)
+      fetch(listaDigimonCorta) //ojo con la url
       .then(res=> res.json())
       .then(data =>{
-          setDigimones(data.items);
+          setDigimones(data);//aca es data.items para dragon ball Z
           setCargandoDatos(false);
           console.log("constante setDigimones");
           console.log(setDigimones);
@@ -27,55 +41,37 @@ function App() {
         }
       )
       .catch(err=>{
-          console.console.error("no se encontró la lista de digimones", err);
+          console.error("no se encontró la lista de digimones", err);
           setCargandoDatos(false);
         }
       );
-    }
-  );
-
-  //mapeo la data y mando por consola para validar
-  //fetch(listaDigimonLarga)
-  //.then(response => response.json())
-  //.then(data => {
-  //  mappedData = data.map(item => {
-  //      // obtengo datos
-  //      nombreDigimon : item.name
-  //      src : item.image
-  //      // ...intento obtener imagen
-  //  }
-  //);
-  //  // muestro por consola a ver si el fetch anduvo
-  //  console.log(mappedData);
-  //})
-  //.catch(error => {
-  //  console.error('muestro el error:', error);
-  //});
+    }, []);
+    console.log("constante digimones");
+    console.log(digimones);
 
   
   return (
     <div>
       <header>
-        <ComponenteHeaderTest/>
+        <ComponenteHeaderTest>Lista de productos Digimones principiante</ComponenteHeaderTest>
       </header>
-        <Container className = 'mt-4'>
-          <h5>lista de digimones corta</h5>
-          <Row>
-            {digimones.map(char=>(
-              <Col key={char.name} md= {4}>
-                <Card className='m-2'>
-                  
-                  <Card.Img src={char.img}/>
-                  <Card.Body>
-                    <Card.Title> {char.name} </Card.Title>
-                    <Card.Text> producto </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
+      <Container className = 'mt-4'>
+       <h5>lista de digimones corta</h5>
+       <Row>
+         {digimones.map(char=>(
+           <Col key={char.name} md= {4}> {/* revisar el unique key para los digimon o DBZ */}
+             <Card className='m-2'>
+               
+               <Card.Img src={char.img}/>
+               <Card.Body>
+                 <Card.Title> {char.name} </Card.Title>
+                 <Card.Text> producto </Card.Text>
+               </Card.Body>
+             </Card>
+           </Col>
+         ))}
+       </Row>
+     </Container>
     </div>
-
   );
 } export default App;
